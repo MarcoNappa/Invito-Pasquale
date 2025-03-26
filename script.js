504cdb7073cd0c33f5bdf2d2bd35a6fbc9439a2a
MarcoNappa <marcomarconappa@gmail.com>
2025-03-26 23:11:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("tasto").onclick = function ringraziamenti() {
+    document.getElementsByClassName("invito");
+    alert("Questo invito è stato porgrammato grazie agli studi da autodidatta in informatica e sviluppo di programmi dal sottoscritto ' Web Developer ' [ Marco Mastrangelo ]");
+}
+
